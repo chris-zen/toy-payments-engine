cc82c3aa4cd33d89e38fce1e87a43ffd219b6a0b
chris-zen <932644+chris-zen@users.noreply.github.com>
2021-07-06 00:52:19 +0200

    force max precission for output

--- src/io/account.rs
+++ src/io/account.rs
@@ -3,6 +3,8 @@ use serde::Serialize;
 
 use crate::payments::{self, ClientId};
 
+const MAX_PRECISION: u32 = 4;
+
 /// A report on an account state used to serialize into a CSV file
 #[derive(Debug, PartialEq, Serialize)]
 pub struct AccountReport {
@@ -18,12 +20,22 @@ impl From<payments::AccountReport> for AccountReport {
   fn from(account_report: payments::AccountReport) -> Self {
     AccountReport {
       client: account_report.client_id,
-      available: account_report.available,
-      held: account_report.held,
-      total: account_report.total,
+      available: with_max_precission(account_report.available),
+      held: with_max_precission(account_report.held),
+      total: with_max_precission(account_report.total),
       locked: account_report.locked,
     }
   }
+}
+
+fn with_max_precission(mut value: Decimal) -> Decimal {
+  if value.scale() > MAX_PRECISION {
+    value.rescale(MAX_PRECISION);
+  }
+  if value.is_zero() {
+    value = Decimal::ZERO;
+  }
+  value
 }
 
 #[cfg(test)]
@@ -35,7 +47,7 @@ mod tests {
   #[test]
   fn from_payments_account_report() {
     let payments_account_report =
-      payments::AccountReport::new(1, dec!(100), dec!(10), dec!(110), false);
+      payments::AccountReport::new(1, dec!(100.12345), dec!(10.012345), dec!(110.5678), false);
 
     let account_report: AccountReport = payments_account_report.into();
 
@@ -43,11 +55,26 @@ mod tests {
       account_report,
       AccountReport {
         client: 1,
-        available: dec!(100),
-        held: dec!(10),
-        total: dec!(110),
+        available: dec!(100.1235),
+        held: dec!(10.0123),
+        total: dec!(110.5678),
         locked: false
       }
     )
+  }
+
+  #[test]
+  fn with_max_precission_rescales() {
+    let cases = vec![
+      (dec!(0.00), "0"),
+      (dec!(0.00000), "0"),
+      (dec!(0.00004), "0"),
+      (dec!(0.00005), "0.0001"),
+      (dec!(1.23456789), "1.2346"),
+    ];
+
+    for (input, expected) in cases {
+      assert_eq!(format!("{}", with_max_precission(input)).as_str(), expected);
+    }
   }
 }
